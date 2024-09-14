69ddbdef9fa9b87963267d69a6956ce7d39ba8ac
Nem Tudo <gf98289@gmail.com>
2024-09-14 20:34:49 -0300

    fix: jwt algorithms

--- app.js
+++ app.js
@@ -923,7 +923,7 @@ app.listen(process.env.PORT, () => {
 
 function verifyJWT(token, key) {
 	try {
-		const decoded = jwt.verify(token, key);
+		const decoded = jwt.verify(token, key, {algorithms: ["ES256"]});
 		return {
 			error: false,
 			data: decoded
